f2bd6afaabbcbf1342f6b1f076204a9e3b5e765b
Davide Lugli <luglidavide.web@gmail.com>
2020-01-09 15:20:07 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@
 // Per i numeri che sono sia multipli di 3
 // che di 5 stampi FizzBuzz.
 // Stampiamo come sempre prima in console e poi su html
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 == 0) {
+    console.log('Fizz');
+  } else if (i % 5 == 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
