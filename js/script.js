e30c219944b2ca3a820b5b4bf42149377b8f0f28
Davide Lugli <luglidavide.web@gmail.com>
2020-01-09 15:23:40 +0100

    anche con while

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ for (var i = 1; i <= 100; i++) {
     console.log(i);
   }
 }
+
+
+// con il while
+// var i = 1;
+// while (i <= 100) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log('FizzBuzz');
+//   } else if (i % 3 == 0) {
+//     console.log('Fizz');
+//   } else if (i % 5 == 0) {
+//     console.log('Buzz');
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
